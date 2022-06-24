c9f20235cf82d56fddc084076e2abf7549755547
Liran Goldman <liran12369@gmail.com>
2022-06-24 17:25:48 +0300

    Basic grid with color change when over & clicked

---
+++ script.js
@@ -0,0 +1,32 @@
+const container = document.getElementById("container");
+let mouseDown = false;
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+
+let gridCount = 16;
+
+for (let i = 1; i <= gridCount; i++) {
+    for (let j = 1; j <= gridCount; j++) {
+        console.log('added');
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.width = String(600 / (gridCount)) + "px";
+        block.style.height = String(600 / (gridCount)) + "px";
+        block.style.gridColumnStart = `${j}`
+        block.style.gridRowStart = `${i}`
+        console.log(block.style.getPropertyValue('height'))
+        //console.log(block.style.getPropertyValue('minHeight'))
+
+        container.append(block);
+    }
+}
+
+const blocks = document.querySelectorAll(".block");
+blocks.forEach(block => {
+    block.addEventListener('mouseover', () => {
+        console.log(mouseDown)
+        if (mouseDown) {
+            block.style.backgroundColor = "gray"
+        }
+    });
+});
