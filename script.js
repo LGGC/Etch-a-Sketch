27c36075b0dfadc315c26e8d773b9b5a58694365
LGGC <liran12369@gmail.com>
2022-06-28 11:10:19 +0300

    organize the code into functions and add grid size slide

--- script.js
+++ script.js
@@ -3,37 +3,44 @@ let mouseDown = false;
 document.body.onmousedown = () => mouseDown = true;
 document.body.onmouseup = () => mouseDown = false;
 
-let gridCount = 16;
+blocks = document.querySelectorAll(".block");
 
-for (let i = 1; i <= gridCount; i++) {
-    for (let j = 1; j <= gridCount; j++) {
-        console.log('added');
-        const block = document.createElement('div');
-        block.classList.add('block');
-        block.style.backgroundColor = "transparent";
-        block.style.width = String(600 / (gridCount)) + "px";
-        block.style.height = String(600 / (gridCount)) + "px";
-        block.style.gridColumnStart = `${j}`
-        block.style.gridRowStart = `${i}`
-        console.log(block.style.getPropertyValue('height'))
-        //console.log(block.style.getPropertyValue('minHeight'))
+setupGrid(16);
 
-        container.append(block);
-    }
-}
-
-const blocks = document.querySelectorAll(".block");
-blocks.forEach(block => {
-    block.addEventListener('mouseover', () => {
-        
-        console.log(mouseDown)
-        if (mouseDown) {
-            const colorPicker = document.getElementById("color-picker").value;
-            block.style.backgroundColor = colorPicker;
-        }
-    });
+const sizeSlide = document.getElementById("size");
+sizeSlide.addEventListener('change', (e) => {
+    container.innerHTML = '';
+    setupGrid(e.target.value);
 });
 
+function setupGrid(size) {
+    for (let i = 1; i <= size; i++) {
+        for (let j = 1; j <= size; j++) {
+            console.log('added');
+            const block = document.createElement('div');
+            block.classList.add('block');
+            block.style.backgroundColor = "transparent";
+            block.style.width = String(600 / (size)) + "px";
+            block.style.height = String(600 / (size)) + "px";
+            block.style.gridColumnStart = `${j}`
+            block.style.gridRowStart = `${i}`
+            console.log(block.style.getPropertyValue('height'))
+            //console.log(block.style.getPropertyValue('minHeight'))
+    
+            container.append(block);
+            block.addEventListener('mouseover', () => {
+        
+                console.log(mouseDown)
+                if (mouseDown) {
+                    const colorPicker = document.getElementById("color-picker").value;
+                    block.style.backgroundColor = colorPicker;
+                }
+            });
+
+        }
+    }
+    blocks = document.querySelectorAll(".block");
+}
 
 const clear = document.getElementById("clear");
 clear.addEventListener('click', () => {  
